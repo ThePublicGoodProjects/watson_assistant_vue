af07d5fc56d33c0a3fc9e9a89768e3a9115a3131
cam-pgp <cam.wiley@publicgoodprojects.org>
2020-04-03 12:05:28 -0700

    added env variables for testing

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,42 @@
 <template>
-    <div class="chat-container">
-        <chat :options="options"></chat>
-    </div>
+  <div class="chat-container">
+    <chat :options="options"></chat>
+  </div>
 </template>
 
 <script>
-    import 'url-search-params-polyfill';
-    import Chat from './components/Chat.vue';
+  import 'url-search-params-polyfill'
+  import Chat from './components/Chat.vue'
 
-    export default {
-        name      : 'chat-module',
-        components: {
-            Chat
+  export default {
+    name: 'chat-module',
+    components: {
+      Chat,
+    },
+    data () {
+      let urlParams = new URLSearchParams(window.location.search),
+        debugMode = urlParams.get('test-mode') === 'debug',
+        botMode = urlParams.get('lang-mode') === 'default' ? 'default' : 'layla'
+
+      return {
+        options: {
+          env: process.env.NODE_ENV || 'production',
+          debug: debugMode,
+          language: botMode,
+          mode: 'web',
         },
-        data() {
-            let urlParams = new URLSearchParams(window.location.search),
-                debugMode = urlParams.get('test-mode') === 'debug',
-                botMode   = urlParams.get('lang-mode') === 'default' ? 'default' : 'layla';
-
-            return {
-                options: {
-                    debug   : debugMode,
-                    language: botMode,
-                    mode    : 'web'
-                },
-            };
-        }
-    };
+      }
+    },
+  }
 </script>
 
 <style>
-    #app {
-        font-family             : 'Avenir', Helvetica, Arial, sans-serif;
-        -webkit-font-smoothing  : antialiased;
-        -moz-osx-font-smoothing : grayscale;
-        text-align              : center;
-        color                   : #2c3e50;
-        margin-top              : 60px;
-    }
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -1,160 +1,162 @@
 <template>
-    <div class="chat" :class="debug ? 'debug' : ''">
-        <div class="chat-inner">
-            <div class="chat-scroll-container">
-                <chat-message-list :messages="messages"></chat-message-list>
-            </div>
-            <chat-message-input @changeInput="changeInput" @submitMessage="addRequest"></chat-message-input>
-        </div>
-        <div class="chat-analysis" v-if="debug">
-            <div class="info">Min. Threshold: {{ threshold * 100 }}%</div>
-            <ul>
-                <li v-for="(intent, index) in intents" v-bind:key="index">
-                    <div class="message">{{ intent.message }}</div>
-                    <div v-for="(intentVal, indexTwo) in intent.intents" v-bind:key="indexTwo">
-                        <span>{{ intentVal.intent }}</span>: <span>{{ Math.round(intentVal.confidence * 100) }}%</span></div>
-                </li>
-            </ul>
-        </div>
+  <div class="chat" :class="debug ? 'debug' : ''">
+    <div class="chat-inner">
+      <div class="chat-scroll-container">
+        <chat-message-list :messages="messages"></chat-message-list>
+      </div>
+      <chat-message-input @changeInput="changeInput" @submitMessage="addRequest"></chat-message-input>
     </div>
+    <div class="chat-analysis" v-if="debug">
+      <div class="info">Min. Threshold: {{ threshold * 100 }}%</div>
+      <ul>
+        <li v-for="(intent, index) in intents" v-bind:key="index">
+          <div class="message">{{ intent.message }}</div>
+          <div v-for="(intentVal, indexTwo) in intent.intents" v-bind:key="indexTwo">
+            <span>{{ intentVal.intent }}</span>
+            :
+            <span>{{ Math.round(intentVal.confidence * 100) }}%</span>
+          </div>
+        </li>
+      </ul>
+    </div>
+  </div>
 </template>
 
 <script>
-    /* eslint-disable no-unused-vars, no-console */
-    import 'current-script-polyfill';
-    import chatMessageList from './ChatMessageList.vue';
-    import chatMessageInput from './ChatMessageInput.vue';
-    import Api from '../watson-api.js';
-    import '../assets/sass/app.scss';
-    import GtmEvents from '../assets/js/gtm-events.js';
+  /* eslint-disable no-unused-vars, no-console */
+  import 'current-script-polyfill'
+  import chatMessageList from './ChatMessageList.vue'
+  import chatMessageInput from './ChatMessageInput.vue'
+  import Api from '../watson-api.js'
+  import '../assets/sass/app.scss'
+  import GtmEvents from '../assets/js/gtm-events.js'
 
-    let settings = {
-        pauseDelay                  : 0,
-        firstInteractionMessageDelay: 30000,
+  let settings = {
+      pauseDelay: 0,
+      firstInteractionMessageDelay: 30000,
     },
-        gtmEvent = GtmEvents();
+    gtmEvent = GtmEvents()
 
-    export default {
-        name      : "Chat",
-        components: {
-            chatMessageList,
-            chatMessageInput
+  export default {
+    name: 'Chat',
+    components: {
+      chatMessageList,
+      chatMessageInput,
+    },
+    props: {
+      options: {
+        type: Object,
+        default: function () {
+          return {}
         },
-        props     : {
-            options: {
-                type   : Object,
-                default: function () {
-                    return {};
-                }
+      },
+    },
+    data () {
+      return {
+        messages: [],
+        intents: [],
+        debug: this.options.debug || false,
+        language: this.options.language || 'default',
+        mode: this.options.mode || 'web',
+        env: this.options.env || 'production',
+        threshold: Api.CONFIDENCE_THRESHOLD,
+        firstInteraction: false,
+      }
+
+    },
+    created () {
+
+    },
+    mounted () {
+      let vm = this,
+        readyEvent = new Event('chatbot.ready')
+
+      this.sendRequest('', {
+        'bot_lang': this.language,
+        'bot_mode': this.mode,
+      }, true)
+
+      document.dispatchEvent(readyEvent)
+
+      setTimeout(function () {
+        if (!vm.firstInteraction) {
+          vm.sendRequest('How can I use this system', false, true)
+        }
+      }, settings.firstInteractionMessageDelay)
+    },
+    methods: {
+      sendRequest: function (message, context, noLog) {
+        const vm = this
+        noLog = noLog || false
+        Api.sendRequest(message, context || {}, noLog).then(function () {
+          let intents = Api.getResponseIntents(),
+            responses = Api.getResponsePayload()
+          if (message !== Api.UNKNOWN_MESSAGE) {
+            vm.setIntents(message, intents)
+          }
+          if (!intents.length || intents[0].confidence > Api.CONFIDENCE_THRESHOLD) {
+            if (intents.length) {
+              gtmEvent('layla-chat', 'user-intent', intents[0].intent)
             }
-        },
-        data() {
-            return {
-                messages        : [],
-                intents         : [],
-                debug           : this.options.debug || false,
-                language        : this.options.language || 'default',
-                mode            : this.options.mode || 'web',
-                threshold       : Api.CONFIDENCE_THRESHOLD,
-                firstInteraction: false
-            };
-
-        },
-        created() {
-
-        },
-        mounted() {
-            let vm = this,
-                readyEvent = new Event('chatbot.ready');
-
-            this.sendRequest('', {
-                'bot_lang': this.language,
-                'bot_mode': this.mode
-            }, true);
-
-            document.dispatchEvent(readyEvent);
+            vm.setResponse(responses)
+          } else {
+            vm.sendRequest(Api.UNKNOWN_MESSAGE, context, noLog)
+          }
+        })
+      },
+      setIntents: function (message, intents, index) {
+        if (intents.length) {
+          this.intents.unshift({
+            message: message,
+            intents: intents,
+          })
+        }
+      },
+      setResponse: function (responses, index) {
+        let response,
+          vm = this
+        index = index || 0
+        if (index < responses.length) {
+          response = responses[index]
+          if (response.response_type === 'pause') {
+            // setTimeout(function () {
+            //     vm.addMessage(response);
+            // }, settings.pauseDelay);
+            vm.addMessage(response)
 
             setTimeout(function () {
-                if (!vm.firstInteraction) {
-                    vm.sendRequest('How can I use this system', false, true);
-                }
-            }, settings.firstInteractionMessageDelay);
-        },
-        methods   : {
-            sendRequest   : function (message, context, noLog) {
-                const vm = this;
-                noLog = noLog || false;
-                Api.sendRequest(message, context || {}, noLog).then(function () {
-                    let intents   = Api.getResponseIntents(),
-                        responses = Api.getResponsePayload();
-                    if (message !== Api.UNKNOWN_MESSAGE) {
-                        vm.setIntents(message, intents);
-                    }
-                    if (!intents.length || intents[0].confidence > Api.CONFIDENCE_THRESHOLD) {
-                        if (intents.length) {
-                            gtmEvent('layla-chat', 'user-intent', intents[0].intent);
-                        }
-                        vm.setResponse(responses);
-                    }
-                    else {
-                        vm.sendRequest(Api.UNKNOWN_MESSAGE, context, noLog);
-                    }
-                });
-            },
-            setIntents    : function (message, intents, index) {
-                if (intents.length) {
-                    this.intents.unshift({
-                        message: message,
-                        intents: intents
-                    });
-                }
-            },
-            setResponse   : function (responses, index) {
-                let response,
-                    vm = this;
-                index  = index || 0;
-                if (index < responses.length) {
-                    response = responses[index];
-                    if (response.response_type === 'pause') {
-                        // setTimeout(function () {
-                        //     vm.addMessage(response);
-                        // }, settings.pauseDelay);
-                        vm.addMessage(response);
+              vm.setResponse(responses, index + 1)
+            }, response.time + settings.pauseDelay)
+          } else {
+            vm.addMessage(response)
+            vm.setResponse(responses, index + 1)
+          }
 
-                        setTimeout(function () {
-                            vm.setResponse(responses, index + 1);
-                        }, response.time + settings.pauseDelay);
-                    }
-                    else {
-                        vm.addMessage(response);
-                        vm.setResponse(responses, index + 1);
-                    }
-
-                }
-            },
-            scrollToBottom: function () {
-                var scrollingChat       = this.$el.querySelector('.chat-scroll-container');
-                scrollingChat.scrollTop = scrollingChat.scrollHeight;
-            },
-            addRequest    : function (message) {
-
-                this.addMessage({
-                    user_response: true,
-                    response_type: 'text',
-                    text         : message
-                });
-                this.sendRequest(message, {}, this.debug);
-            },
-            addMessage    : function (message) {
-                this.messages.push(message);
-
-            },
-            changeInput   : function () {
-                this.firstInteraction = true;
-            }
         }
+      },
+      scrollToBottom: function () {
+        var scrollingChat = this.$el.querySelector('.chat-scroll-container')
+        scrollingChat.scrollTop = scrollingChat.scrollHeight
+      },
+      addRequest: function (message) {
 
-    };
+        this.addMessage({
+          user_response: true,
+          response_type: 'text',
+          text: message,
+        })
+        this.sendRequest(message, {}, this.env !== 'production' || this.debug)
+      },
+      addMessage: function (message) {
+        this.messages.push(message)
+
+      },
+      changeInput: function () {
+        this.firstInteraction = true
+      },
+    },
+
+  }
 </script>
 
 <style lang="scss">
